9ca3dd363e0bd501fe994b0274b28b595363611a
Manuel Ozan <manuozan97@gmail.com>
2023-02-04 14:12:57 -0300

    mejora en el carrito

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function add(product, price) {
     console.log(product, price);
     products.push(product);
     total = total + price;
-    document.getElementById("checkout").innerHTML = `Pagar $${total}`
+    document.getElementById("checkout").innerHTML = `Carrito $${total}`
 }
 
 function pay() {
